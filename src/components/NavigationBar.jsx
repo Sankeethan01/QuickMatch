9506f47392c04d0d181a8db73236c3cffca817e4
Manula Alahakoon <manulaalahakoon@gmail.com>
2024-06-13 16:03:21 +0530

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import logo from '../assets/logo.png';
+import Logo from "../assets/logo.png";
 
 const NavigationBar = () => {
   return (
@@ -12,20 +12,21 @@ const NavigationBar = () => {
           <Navbar.Brand href="#home">
             <img
               alt="Logo"
-              src={logo}
+              src={Logo}
               width="60"
               height="60"
               className="d-inline-block align-center"
-            />{' '}
-           QuickMatch
+            />
+            QuickMatch
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">Home</Nav.Link>
-              <Nav.Link href="#pricing">About</Nav.Link>
               <NavDropdown title="Services" id="collapsible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Electric & Electronic</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">
+                  Electric & Electronic
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.2">
                   Event Management
@@ -35,6 +36,8 @@ const NavigationBar = () => {
                   Construction
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#contact">Contact Us</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="#deets">Sign In</Nav.Link>
