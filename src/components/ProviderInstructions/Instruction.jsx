9cedd26438834e0ea8282c6de4af77824ad4b072
Sankeethan01 <sankee26july@gmail.com>
2024-07-02 15:24:09 +0530

    new commit

--- src/components/ProviderInstructions/Instruction.jsx
+++ src/components/ProviderInstructions/Instruction.jsx
@@ -15,14 +15,15 @@ const Instruction = ({ closeInstruct }) => {
               Welcome to QuickMatch! Please adhere to these guidelines for a
               seamless experience: Do not share your mobile number with
               customers; all interactions must be conducted through the
-              application. Even with repeat customers, ensure all communications
-              are within the platform, as direct connections outside the app are
-              strictly prohibited. Use email only if necessary to contact
-              customers. If you are unable to attend to a customer,
-              decline their request within two weeks of the booking date.
-              Following these instructions helps maintain the integrity and
-              reliability of our platform. Thank you for your cooperation and
-              professionalism.
+              application. You should submit a pdf consists of proofs for ensure
+              you are a provider. You can login after you are verified. Even
+              with repeat customers, ensure all communications are within the
+              platform, as direct connections outside the app are strictly
+              prohibited. Use email only if necessary to contact customers. If
+              you are unable to attend to a customer, decline their request
+              within two weeks of the booking date. Following these instructions
+              helps maintain the integrity and reliability of our platform.
+              Thank you for your cooperation and professionalism.
             </p>
             <h6>
               <input type="checkbox" name="" id="" /> I agree the terms of
