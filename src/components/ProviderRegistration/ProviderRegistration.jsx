010c20eb6311acf449b3354cc0a37f50da293661
ADeluxshana <dinaamal1012@gmail.com>
2024-10-20 11:21:43 +0530

    footer css updated

--- src/components/ProviderRegistration/ProviderRegistration.jsx
+++ src/components/ProviderRegistration/ProviderRegistration.jsx
@@ -180,6 +180,13 @@ const ProviderRegistration = ({ providerData, openFinalReg }) => {
                     <MiscellaneousServicesIcon className="icon" />
                   </div>
                 )}
+
+
+                <div style={{marginBottom:"-40px",marginTop:"40px",marginLeft:"15px"}}>
+                  <p>Proof:</p>
+                </div>
+
+                
                 <div className="input_box">
                   <input
                     className="input_proof"
